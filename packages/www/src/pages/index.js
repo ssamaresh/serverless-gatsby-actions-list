0d3b018567932f97c3d7b675f5db7dcd76233f23
Samaresh Sridhar <ssamaresh1990@gmail.com>
2021-01-21 23:14:32 -0500

    Added scripts, prettier, pre commit hooks

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -2,21 +2,23 @@ import React from 'react';
 import { Container, Heading, Button, Flex } from 'theme-ui'
 
 export default props => {
-    return (
-        <Container>
-            <Flex sx={{
-                flexDirection: 'column',
-                alignItems: 'center',
-                justifyContent: 'center',
-                padding: 3
-            }}
-            >
-                <Heading as='h1'>Get Ready! Take Action.</Heading>
-                <Button sx={{
-                    mx: 'auto',
-                    my: 3
-                }}>Log In</Button>
-            </Flex>
-        </Container>
-    )
+  return (
+    <Container>
+      <Flex sx={{
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 3
+      }}
+    >
+      <Heading as='h1'>Get Ready! Take Action.</Heading>
+      <Button sx={{
+        mx: 'auto',
+        my: 3
+      }}>
+        Log In
+      </Button>
+    </Flex>
+  </Container>
+  )
 };
