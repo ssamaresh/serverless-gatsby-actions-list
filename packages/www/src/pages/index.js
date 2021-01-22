9c40be327ea971b0c90098d0e4f6a7537e596834
Samaresh Sridhar <ssamaresh1990@gmail.com>
2021-01-21 23:28:34 -0500

    Added netlify identity

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Container, Heading, Button, Flex } from 'theme-ui'
+import netlifyIdentity from 'netlify-identity-widget';
 
-export default props => {
+const App =  props => {
+
+  useEffect(() => {
+    netlifyIdentity.init({});
+  });
+
+  const handleLogin = () => {
+    netlifyIdentity.open();
+  };
+
   return (
     <Container>
       <Flex sx={{
@@ -12,13 +22,18 @@ export default props => {
       }}
     >
       <Heading as='h1'>Get Ready! Take Action.</Heading>
-      <Button sx={{
-        mx: 'auto',
-        my: 3
-      }}>
+      <Button
+        sx={{
+          mx: 'auto',
+          my: 3
+        }}
+        onClick={handleLogin}
+      >
         Log In
       </Button>
     </Flex>
   </Container>
   )
 };
+
+export default App;
