223b573c15f8eedf98aa4ccd38f4992bfe98057b
Samaresh Sridhar <ssamaresh1990@gmail.com>
2021-01-21 23:02:40 -0500

    Added basic UI Theme

---
+++ packages/www/gatsby-browser.js
@@ -0,0 +1,3 @@
+const wrapRootElement = require('./wrapper');
+
+exports.wrapRootElement = wrapRootElement;
--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Container, Heading, Button, Flex } from 'theme-ui'
 
 export default props => {
     return (
-        <div>
-            <h1>Actions List</h1>
-        </div>
+        <Container>
+            <Flex sx={{
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                padding: 3
+            }}
+            >
+                <Heading as='h1'>Get Ready! Take Action.</Heading>
+                <Button sx={{
+                    mx: 'auto',
+                    my: 3
+                }}>Log In</Button>
+            </Flex>
+        </Container>
     )
 };
